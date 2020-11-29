c96970f7532cc030abe25fc4f7a792e181e7c7df
plally <11771225+plally@users.noreply.github.com>
2020-11-28 20:28:47 -0500

    spacing and fix bugs

--- lua/autorun/client/cl_cfc_optional_addon_ui.lua
+++ lua/autorun/client/cl_cfc_optional_addon_ui.lua
@@ -2,22 +2,19 @@
 local checkboxes = {}
 
 local function populatePanel( form )
-    -- make  checkboxes
+    -- make checkboxes
     for id, _ in pairs( CFC_ClientAddonLoader.allowedAddons ) do
-        
-        local checkbox = form:CheckBox( "Name loading" )
+        local checkbox = form:CheckBox( "N/A" )
         checkbox.addonId = id
-        checkbox:Dock( FILL )
         checkbox:SetChecked( CFC_ClientAddonLoader.enabledAddons[id] == true )
-        steamworks.FileInfo( id, function( result ) 
-            checkbox.Label:SetText( string.format("%s : %s", result.title, id) )
+
+        steamworks.FileInfo( id, function( result )
+            checkbox.Label:SetText( string.format( "%s : %s", result.title, id ) )
             checkbox.Label:SizeToContents()
-            
         end )
-        
-        table.insert(checkboxes, checkbox)
+        table.insert( checkboxes, checkbox )
     end
-    
+
     -- make apply button
     local button = form:Button( "Apply" )
     function button:DoClick()
@@ -32,11 +29,11 @@ local function populatePanel( form )
     end
     form:Help( "* Restart is required when disabling addons" )
 end
-makeForm()
 
 hook.Add( "AddToolMenuCategories", "CFC_HTTP_ListManager", function()
     spawnmenu.AddToolCategory( "Options", "OptionalAddons", "Optional Addons" )
 end )
+
 hook.Add( "PopulateToolMenu", "CFC_HTTP_ListManager", function()
     spawnmenu.AddToolMenuOption( "Options", "OptionalAddons", "optional_addons", "OptionalAddons", "", "", function( panel )
         populatePanel( panel )
